530ab14d4a0f70f6775c4f636d5f5374b06ed5ab
Darko Komljenovic <dkomljenovic@zoho.com>
2020-03-13 08:12:24 +0100

    Test servo aplication

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,31 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+
+Servo myservo;
+int value = 0;
+
+void servo_manipulation(){
+	value +=10 ;
+	
+	if (value > 180){
+		value = 0;
+	}
+	Serial.println(value);
+	myservo.write(value); 
+}
 
 void setup() {
+	Serial.begin(115200);
 	pinMode (LED_BUILTIN, OUTPUT);
+	myservo.attach(23);
 }
 
 void loop() {
 	digitalWrite(LED_BUILTIN, HIGH);
+	servo_manipulation();
 	delay(1000);
 	digitalWrite(LED_BUILTIN, LOW);
+	servo_manipulation();
 	delay(1000);
 }
