d9ad30cd52a98cfeaeecb003a3ee9730662aea56
Darko Komljenovic <dkomljenovic@zoho.com>
2020-03-14 18:59:08 +0100

    Encoder, display, speed sensor works hardware

--- src/defines.h
+++ src/defines.h
@@ -1,6 +1,18 @@
 //********************DEFINE PINS*********************//
 #define SERVO_PIN 13
 #define SPEED_READ_PIN 12
+#define ENCODER_PIN_1 14
+#define ENCODER_PIN_2 27
+#define ENCODER_PIN_SW 26
+#define ENCODER_PIN_VCC -1
+#define TFT_CS -1
+#define TFT_DC 33
+#define TFT_RST 25
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+#define SPI_SS 5
+#define POWER_SPEED_SENSOR 32
 //*****************END DEFINE PINS********************//
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,50 @@
 #include "defines.h"
 #include <Arduino.h>
-#include <Servo.h>
+#include <SPI.h>
 #include <thread>
 #include <chrono>
+#include "Servo.h"
+#include "AiEsp32RotaryEncoder.h"
+#include "Adafruit_ST7789.h"
+
+#define DEBUG
+#undef DEBUG
+#ifdef DEBUG
+#define DEBUG_PRINT(t_input) print_to_screenln(t_input);
+#else
+#define DEBUG_PRINT(t_input);
+#endif
 
 Servo myservo;
 int enc_value = 55;
 std::chrono::system_clock::time_point now;
+bool start_centrifuge = false;
 
+//Rotary encoder
+AiEsp32RotaryEncoder my_enc = AiEsp32RotaryEncoder(ENCODER_PIN_1, ENCODER_PIN_2, ENCODER_PIN_SW, ENCODER_PIN_VCC);
+int32_t rpm_setp  = -999;
+
+//Display
+Adafruit_ST7789 my_display = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+// Print to screen
+void clear_screen(){
+	my_display.setCursor(0,0);
+	my_display.fillScreen(ST77XX_BLACK);
+}
+void print_to_screenln(String t_inp_string){
+	Serial.println(t_inp_string);
+	my_display.println(t_inp_string);
+}
+void print_to_screen(String t_inp_string){
+	Serial.print(t_inp_string);
+	my_display.print(t_inp_string);
+}
+//end print to screen
+
+//Read speed
+int speed_read_value;
 void read_speed(){
 	//Speed sensor init
-	int speed_read_value;
 	bool was_covered = false;
 	long speed_counter = 0;
 	int time_passed;
@@ -27,44 +61,73 @@ void read_speed(){
 		now = std::chrono::system_clock::now();
 		time_passed = std::chrono::duration_cast<std::chrono::milliseconds>(now - speed_read_at_time).count();
 		if (time_passed >= 1000){
-			Serial.print("Sensor speeed ::");
-			Serial.println((speed_counter * 60000)/ time_passed);
+			print_to_screenln("Sensor spd::" + String((speed_counter * 60000)/ time_passed) + " rpm");
 			speed_read_at_time = std::chrono::system_clock::now();
 			speed_counter = 0;
 		}
-
-		
-		std::this_thread::sleep_for(speed_timer_sleep);
+		delay(1);
 	}
 }
+//Create a task for speed read
+std::thread read_speed_thread(read_speed);
+//END Read speed
+
 
 void servo_manipulation(){
-	enc_value +=1 ;
 	
 	if (enc_value > 65){
 		enc_value = 55;
 	}
-	//Serial.println(enc_value);
+	print_to_screenln("Enc value :: " + String(enc_value));
 	myservo.write(enc_value); 
 }
 
-	//Create a task for speed read
-    std::thread read_speed_thread(read_speed);
+
 void setup() {
-	//Set the servo
-	myservo.attach(SERVO_PIN);
 	//Start the serial readout
 	Serial.begin(115200);
-	
-	
+	//Start the spi
+	SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI, SPI_SS);
+	//Start the display
+	my_display.init(240, 240, SPI_MODE2);
+	my_display.fillScreen(ST77XX_BLACK);
+	my_display.setRotation(3);
+	my_display.setTextSize(3);
+	//Set the servo
+	myservo.attach(SERVO_PIN);
+	//we must initialize rorary encoder 
+	my_enc.begin();
+	my_enc.setup([]{my_enc.readEncoder_ISR();});
+	//optionally we can set boundaries and if values should cycle or not
+	my_enc.setBoundaries(0, 10, true); //minValue, maxValue, cycle values (when max go to min and vice versa)
+		
 	pinMode (LED_BUILTIN, OUTPUT);
+	pinMode (POWER_SPEED_SENSOR, OUTPUT);
+	digitalWrite(POWER_SPEED_SENSOR, HIGH);
 }
 
 void loop() {
-	digitalWrite(LED_BUILTIN, HIGH);
-	servo_manipulation();
-	delay(3000);
-	digitalWrite(LED_BUILTIN, LOW);
-	servo_manipulation();
-	delay(3000);
+	static int32_t my_enc_delta = my_enc.readEncoder();
+	DEBUG_PRINT("Read data ::" + String (my_enc_delta));
+  	if (my_enc.currentButtonState() == BUT_PUSHED) {
+		print_to_screenln("My button BUT_PUSHED");
+		start_centrifuge = !start_centrifuge;
+		
+		enc_value +=1 ; // delete me
+	}
+
+	if (start_centrifuge){
+		servo_manipulation();
+	} else {
+		myservo.write(0);
+	}
+
+	if (my_enc_delta != 0) {
+		rpm_setp += my_enc_delta;
+		print_to_screenln("Speed is: " + String(rpm_setp) + "rpm");
+	}
+
+	delay(100);
+	my_enc.enable();
+	clear_screen();
 }
